9b8cf3a2e126611d758692ad2deb218cb8a38940
Papa Attrams <48452451+attrams@users.noreply.github.com>
2026-01-01 11:07:00 +0000

    Add tab views
    - Add the chats, Updates, Communities and Calls tabs.

---
+++ Echo/Core/Tabbar/View/MainTabbarView.swift
@@ -0,0 +1,85 @@
+//
+//  MainTabbarView.swift
+//  Echo
+//
+//  Created by Attrams on 01/01/2026.
+//
+
+import SwiftUI
+
+struct MainTabbarView: View {
+    @State private var selectedTab: Int = 0
+
+    var body: some View {
+        TabView {
+            Text("Inbox")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "bubble.fill")
+                            .environment(
+                                \.symbolVariants,
+                                selectedTab == 0 ? .fill : .none
+                            )
+
+                        Text("Chats")
+                    }
+                    .onAppear {
+                        selectedTab = 0
+                    }
+                }
+
+            Text("Updates")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "dial.low")
+                            .environment(
+                                \.symbolVariants,
+                                selectedTab == 1 ? .fill : .none
+                            )
+
+                        Text("Updates")
+                    }
+                    .onAppear {
+                        selectedTab = 1
+                    }
+                }
+
+            Text("Communities")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.3")
+                            .environment(
+                                \.symbolVariants,
+                                selectedTab == 2 ? .fill : .none
+                            )
+
+                        Text("Communities")
+                    }
+                    .onAppear {
+                        selectedTab = 2
+                    }
+                }
+
+            Text("Calls")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "phone")
+                            .environment(
+                                \.symbolVariants,
+                                selectedTab == 3 ? .fill : .none
+                            )
+
+                        Text("Calls")
+                    }
+                    .onAppear {
+                        selectedTab = 3
+                    }
+                }
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabbarView()
+}
